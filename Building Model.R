d852e6f168933f29e19e19fbb78ee9960b8b2e3a
Jackson Dugger <jrd3946@educcomp01.ccbb.utexas.edu>
2022-11-29 12:54:39 -0600

    added stuff

--- Building Model.R
+++ Building Model.R
@@ -5,6 +5,7 @@ require(MASS)
 require(neuralnet)
 require(fdm2id)
 require(nnet)
+require(Metrics)
 
 fires <- read_csv("forestfires.csv")
 head(fires)
@@ -22,7 +23,7 @@ test <- fires[samp==2,]
 
 # change these kernels and types to minimize RMSE
 svr_model_1 <- svm(area_log~., train)
-svr_model_2 <- svm(area_log~temp+RH+wind+rain, train)
+svr_model_2 <- svm(area_log~temp+RH+DMC+rain, train)
 
 pred_y_svr_1 <- predict(svr_model_1, test)
 pred_y_svr_2 <- predict(svr_model_2, test)
@@ -43,7 +44,7 @@ intercept_1 <- svr_model_1$rho
 coefs_2 <- t(svr_model_2$coefs) %*% svr_model_2$SV
 intercept_2 <- svr_model_2$rho
 
-RMSE_1 <- rmse(pred_y_svr_1,test$area_log)
+RMSE_1 <- rmse(predicted=pred_y_svr_1,actual=test$area_log)
 RMSE_1
 RMSE_2 <- rmse(pred_y_svr_2,test$area_log)
 RMSE_2
@@ -51,8 +52,18 @@ RMSE_2
 exp(coefs_1)-1
 exp(intercept_1)-1
 
+r2 <- R2(obs=test$area_log,pred=pred_y_svr_1,form='traditional')
+mse_1 <- RMSE_1^2
+
+r2_2 <- R2(obs=test$area_log,pred=pred_y_svr_2,form='traditional')
+
+svr_model_1
+mse_1
+r2
+
 # Try Random Forest Regression now
 
+set.seed(123)
 rf <- randomForest(area_log~temp+RH+DMC+rain, data=train, ntree=47)
 
 rf
@@ -60,9 +71,11 @@ rf
 pred_y_rf <- predict(rf, test)
 
 plot(pred_y_rf, test$area_log)
-lines(x=0:120,y=0:120)
+lines(x=0:50,y=0:50)
 
 summary(rf)
 varImpPlot(rf)
 
 which(rf$mse == min(rf$mse))
+
+R2(obs=test$area_log,pred=pred_y_rf,form='traditional')

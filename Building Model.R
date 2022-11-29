3ca339e3c997b09462f3f90ea89aef29fc7911ac
Jackson Dugger <jrd3946@educcomp01.ccbb.utexas.edu>
2022-11-28 21:27:14 -0600

    model testing

---
+++ Building Model.R
@@ -0,0 +1,68 @@
+require(readr)
+require(randomForest)
+require(caret)
+require(MASS)
+require(neuralnet)
+require(fdm2id)
+require(nnet)
+
+fires <- read_csv("forestfires.csv")
+head(fires)
+
+fires <- na.omit(fires)
+
+fires <- fires %>% mutate_if(is.character, as.factor)
+
+fires <- fires %>% mutate(area_log = log(area+1))
+
+set.seed(1234)
+samp <- sample(2, nrow(fires), replace=T, prob=c(0.7,0.3))
+train <- fires[samp==1,]
+test <- fires[samp==2,]
+
+# change these kernels and types to minimize RMSE
+svr_model_1 <- svm(area_log~., train)
+svr_model_2 <- svm(area_log~temp+RH+wind+rain, train)
+
+pred_y_svr_1 <- predict(svr_model_1, test)
+pred_y_svr_2 <- predict(svr_model_2, test)
+
+plot(pred_y_svr_1,test$area_log)
+lines(x=0:50,y=0:50)
+plot(pred_y_svr_2,test$area_log)
+lines(x=0:50,y=0:50)
+
+plot(exp(pred_y_svr_1)-1,exp(test$area_log)-1,xlim=c(0,30))
+lines(x=0:50, y=0:50)
+plot(exp(pred_y_svr_2)-1,exp(test$area_log)-1,xlim=c(0,20))
+lines(x=0:50, y=0:50)
+
+coefs_1 <- t(svr_model_1$coefs) %*% svr_model_1$SV
+intercept_1 <- svr_model_1$rho
+
+coefs_2 <- t(svr_model_2$coefs) %*% svr_model_2$SV
+intercept_2 <- svr_model_2$rho
+
+RMSE_1 <- rmse(pred_y_svr_1,test$area_log)
+RMSE_1
+RMSE_2 <- rmse(pred_y_svr_2,test$area_log)
+RMSE_2
+
+exp(coefs_1)-1
+exp(intercept_1)-1
+
+# Try Random Forest Regression now
+
+rf <- randomForest(area_log~temp+RH+DMC+rain, data=train, ntree=47)
+
+rf
+
+pred_y_rf <- predict(rf, test)
+
+plot(pred_y_rf, test$area_log)
+lines(x=0:120,y=0:120)
+
+summary(rf)
+varImpPlot(rf)
+
+which(rf$mse == min(rf$mse))
